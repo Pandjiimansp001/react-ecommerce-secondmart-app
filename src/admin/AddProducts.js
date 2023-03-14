e1862713d663683a9bbfde6e55519b4a96a32f3d
Pandjiimansp001 <pandjiimansp001@gmail.com>
2023-03-14 11:25:29 +0700

    update

--- src/admin/AddProducts.js
+++ src/admin/AddProducts.js
@@ -1,35 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 
 const AddProducts = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredShortDesc, setEnteredShortDesc] = useState("");
+  const [enteredDescription, setEnteredDescription] = useState("");
+  const [enteredCategory, setEnteredCategory] = useState("");
+  const [enteredPrice, setEnteredPrice] = useState("");
+  const [enteredProductImg, setEnteredProductImg] = useState(null);
+
+  const AddProduct = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      title: enteredTitle,
+      shortDesc: enteredShortDesc,
+      description: enteredDescription,
+      category: enteredCategory,
+      price: enteredPrice,
+      imgUrl: enteredProductImg,
+    };
+
+    console.log(product);
+  };
+
   return (
     <section>
       <Container>
         <Row>
           <Col lg="12">
             <h4 className="mb-5">Add Product</h4>
-            <Form>
+            <Form onSubmit={AddProduct}>
               <FormGroup className="form__group">
                 <span>Product Title</span>
-                <input type="text" placeholder="Double sofa" />
+                <input
+                  type="text"
+                  placeholder="Double sofa"
+                  value={enteredTitle}
+                  onChange={(e) => setEnteredTitle(e.target.value)}
+                  required
+                />
               </FormGroup>
               <FormGroup className="form__group">
                 <span>Short Description</span>
-                <input type="text" placeholder="lorem...." />
+                <input
+                  type="text"
+                  placeholder="lorem...."
+                  value={enteredShortDesc}
+                  onChange={(e) => setEnteredShortDesc(e.target.value)}
+                  required
+                />
               </FormGroup>
               <FormGroup className="form__group">
                 <span>Description</span>
-                <input type="text" placeholder="Description...." />
+                <input
+                  type="text"
+                  placeholder="Description...."
+                  value={enteredDescription}
+                  onChange={(e) => setEnteredDescription(e.target.value)}
+                  required
+                />
               </FormGroup>
 
               <div className="d-flex align-items-center justify-content-between gap-5">
                 <FormGroup className="form__group w-50">
                   <span>Price</span>
-                  <input type="number" placeholder="$100" />
+                  <input
+                    type="number"
+                    placeholder="$100"
+                    value={enteredPrice}
+                    onChange={(e) => setEnteredPrice(e.target.value)}
+                  />
                 </FormGroup>
                 <FormGroup className="form__group w-50">
                   <span>Category</span>
-                  <select className="w-100 p-2">
+                  <select
+                    className="w-100 p-2"
+                    value={enteredCategory}
+                    onChange={(e) => setEnteredCategory(e.target.value)}
+                    required
+                  >
                     <option value="chair">Chair</option>
                     <option value="sofa">Sofa</option>
                     <option value="mobile">Mobile</option>
@@ -42,11 +92,17 @@ const AddProducts = () => {
               <div>
                 <FormGroup className="form__group">
                   <span>Product Image</span>
-                  <input type="file" />
+                  <input
+                    type="file"
+                    onChange={(e) => setEnteredProductImg(e.target.files[0])}
+                    required
+                  />
                 </FormGroup>
               </div>
 
-              <button className="buy__btn">Add Product</button>
+              <button className="buy__btn" type="submit">
+                Add Product
+              </button>
             </Form>
           </Col>
         </Row>
